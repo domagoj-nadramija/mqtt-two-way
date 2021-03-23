004c1bea3182710f02f9a50be3acbe0af95f707a
Domagoj Nadramija <domagoj.nadramija@t-matix.com>
2021-03-23 14:49:55 +0100

    Format, refactor and make some small changes

--- tcp/iot-device.js
+++ tcp/iot-device.js
@@ -1,46 +1,66 @@
 const net = require("net");
 const fs = require("fs");
 
-const device_id = "test-device-tcp-0001";
+const device_id = "simulated-device-tcp-0001";
 
-console.log(`Starting TCP IoT device <${device_id}>`);
+console.log(`STARTING TCP IOT DEVICE <${device_id}>`);
 
-const config = JSON.parse(fs.readFileSync(__dirname+"/config.json"));
+const config = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
 
-console.log(`Will connect to ${config.server.host}:${config.server.port}`);
+console.log(`WILL CONNECT TO ${config.server.host}:${config.server.port}`);
 
 const client = new net.Socket();
 
 client.connect(config.server.port, config.server.host, function () {
-  console.log("Connected to server");
-  client.write(JSON.stringify({ device_id, message_type: "SIGNUP"}));
+  console.log("CONNECTED TO SERVER");
+  client.write(JSON.stringify({ device_id, message_type: "SIGNUP" }));
   // this simulates the device collecting and sending sensor data
   const dataSendInterval = setInterval(() => {
-    const data = JSON.stringify({ device_id, message_type: "DATA", temp: 23.6, lat: 48.015722, lng: -88.625528});
-    console.log(`Sending data ${data} to server`);
+    const data = JSON.stringify({
+      device_id,
+      message_type: "DATA",
+      temp: 23.6,
+      lat: 48.015722,
+      lng: -88.625528,
+    });
+    console.log(`SENDING DATA: ${data}`);
     client.write(data);
   }, 5000);
 
   client.on("data", (data) => {
-	command = data.toString();
-	console.log("Received command: " + command);
-	const success = execute(command);
-	const commandResp = JSON.stringify({ device_id, message_type: "COMMAND_RESP", command, success});
-	console.log(`Sending command response ${commandResp} to server`);
-	client.write(commandResp);
+    const command = JSON.parse(data.toString());
+    console.log("RECEIVED COMMAND: " + data.toString());
+    const command_result = execute(command.command);
+    const commandResp = JSON.stringify({
+      device_id,
+      message_type: "COMMAND_RESP",
+      command_result,
+      ...command,
+    });
+    console.log(`SENDING COMMAND RESPONSE ${commandResp}`);
+    client.write(commandResp);
   });
 
   client.on("close", () => {
-	console.log("Connection closed");
-	clearInterval(dataSendInterval);
+    console.log("CONNECTION CLOSED");
+    clearInterval(dataSendInterval);
   });
-  
   client.on("error", (error) => {
-	console.log(`Error during connection: ${error.code}`);
-	clearInterval(dataSendInterval);
+    console.log(`ERROR WITH CONNECTION: ${error.code}`);
+    clearInterval(dataSendInterval);
+  });
+  client.on("timeout", () => {
+    console.log(`CONNECTION TIMED-OUT`);
+    clearInterval(dataSendInterval);
   });
 });
 
+// dummy function pretending to execute a command
 function execute(command) {
-  return true;
+  switch (command) {
+    case "PING":
+      return "PONG";
+    default:
+      return "ERROR_UNKNOWN_COMMAND";
+  }
 }
--- tcp/iot-server.js
+++ tcp/iot-server.js
@@ -1,43 +1,44 @@
 const net = require("net");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-const config = JSON.parse(fs.readFileSync(__dirname+"/config.json"));
+const config = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
 
-console.log(`Starting TCP IoT server on port ${config.server.port}`);
+console.log(`STARTING TCP IOT SERVER ON PORT ${config.server.port}`);
 
 const server = net.createServer((socket) => {
   const connPort = socket.remotePort;
-  console.log(`New connection from ${connPort}`);
+  console.log(`NEW CONNECTION FROM ${connPort}`);
 
   // this simulates the user or an automated system sending commands to the device
   const commandSendInterval = setInterval(() => {
-    const command = JSON.stringify({ uuid: uuidv4(), command: "GET_STATUS" });
-    console.log(`Sending command ${command} to ${connPort}`);
+    const command = JSON.stringify({ uuid: uuidv4(), command: "PING" });
+    console.log(`SENDING COMMAND TO ${connPort}: ${command}`);
     socket.write(command);
   }, 10000);
 
   socket.on("data", (data) => {
-    console.log(`Got data from ${connPort}:`, data.toString());
-    try {
-        JSON.parse(data);
-    } catch (error) {
-        console.warn("Data is not valid JSON and will be dropped");
-        return;
-    }
-    console.log("Data is valid JSON and will be stored");
+    console.log(`RECEIVED DATA FROM ${connPort}: ${data.toString()}`);
+
     storeData(data);
   });
   socket.on("close", () => {
-    console.log(`Closing connection to ${connPort}`);
+    console.log(`CLOSING CONNECTION TO ${connPort}`);
     clearInterval(commandSendInterval);
   });
   socket.on("error", (error) => {
-    console.log(`Error with connection to ${connPort}:`, error.code);
+    console.log(`ERROR WITH CONNECTION TO ${connPort}:`, error.code);
+    clearInterval(commandSendInterval);
+  });
+  socket.on("timeout", () => {
+    console.log(`CONNECTION TO ${connPort} TIMED-OUT`);
     clearInterval(commandSendInterval);
   });
 });
 
 server.listen(config.server.port, "127.0.0.1");
 
-function storeData (data) {}
+// dummy function pretending to store data
+function storeData(data) {
+  console.log("Storing data...");
+}
